883c38e9ada5bb73318da14fedbd6e3e1730dd24
Manit Kumar <vikky_manit@yahoo.co.in>
2023-08-01 22:17:15 +0200

    remove localrun file

--- localrun.sh
+++
@@ -1,7 +0,0 @@
-#/bin/sh
-
-export HOSTS=blikk1.hu,blikk2.hu
-export KT_API_HOSTS=https://api.keytiles.com,https://api2.keytiles.com
-export KT_SCRIPT_HOST=https://scripts.keytiles.com
-
-go run main.go
